1b346d273bc60fe37983e3c41ce7fb7004be1559
Omrani7 <mohamed.omrani376@gmail.com>
2024-10-31 17:55:15 +0100

    adding the token service to handle token related logic

--- frontend-ui/src/app/pages/login/login.component.ts
+++ frontend-ui/src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { AuthenticationService } from '../../../../.src/app/services/services/au
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TokenService } from '../../../../.src/app/services/services/token/token.service';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -16,7 +17,9 @@ export class LoginComponent {
   authRequest: AuthenticationRequest = {email: '', password: ''};
   errorMsg: Array<string> = [];
 
-  constructor(private router:Router,private authService:AuthenticationService){
+  constructor(private router:Router,
+    private authService:AuthenticationService ,
+    private tokenService: TokenService ){
 
   }
 
@@ -25,7 +28,8 @@ export class LoginComponent {
     this.authService.authenticate({
       body: this.authRequest
     }).subscribe({
-      next: () => {
+      next: (res) => {
+        this.tokenService.token = res.token as string;
         this.router.navigate(['books']);
       },
       error: (err) => {
